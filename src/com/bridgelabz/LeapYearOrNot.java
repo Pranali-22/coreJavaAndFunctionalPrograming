78f62ef58dc00590bde204065ff766e1679be4e8
Pranali-22 <lpranali22@gmail.com>
2022-09-08 21:38:53 +0530

    Added: No2: Program for leap year

---
+++ src/com/bridgelabz/LeapYearOrNot.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+import java.util.*;
+/**
+ * @author Dell
+ *
+ */
+public class LeapYearOrNot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter year");
+		int year=input.nextInt();
+		
+		if(year>=1000 && year<=9999) {
+			if(year%400==0 || year%4==0 && year%100!=0) {
+				System.out.println(year+" is a leap year");
+			}
+			else {
+				System.out.println(year+" is not a leap year");
+			}
+		}
+		
+		else {
+			System.out.println("Enter 4 digit year");
+		}
+		
+		input.close();
+
+	}
+
+}
